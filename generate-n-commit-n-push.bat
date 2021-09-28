438a5807711c71b798153417675c7d0fa6629a6b
Andrei Punko <andd3dfx@gmail.com>
2021-09-28 19:36:48 +0300

    Add content

--- generate-n-commit-n-push.bat
+++ generate-n-commit-n-push.bat
@@ -1,8 +1,8 @@
 java -jar github-pages-content-generator.jar citations.txt template.html index.html
 
-git fetch
-git rebase origin/master
-
 git add --all
 git commit -m "Add content"
+
+git fetch
+git rebase origin/master
 git push origin master
