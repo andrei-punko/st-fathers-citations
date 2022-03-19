bede090ec604034da5a7fac7a7b215e0f717f3ab
Andrei Punko <andd3dfx@gmail.com>
2022-03-19 15:04:36 +0300

    Add content

--- generate.bat
+++ generate.bat
@@ -5,3 +5,4 @@ java -jar bin/github-pages-content-generator.jar src/citations.txt bin/template.
 java -jar bin/github-pages-content-generator.jar src/novij-zavet-o-carstve-nebesnom.txt bin/template.html novij-zavet-o-carstve-nebesnom.html
 java -jar bin/github-pages-content-generator.jar src/skolko-let.txt bin/template.html skolko-let.html
 java -jar bin/github-pages-content-generator.jar src/apologia-pravoslavia.txt bin/template.html apologia-pravoslavia.html
+java -jar bin/github-pages-content-generator.jar src/monastery-requisites.txt bin/template.html monastery-requisites.html
