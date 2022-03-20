fa6ba494bd3e857ff5d1ddaa6ef7a98f6d8b8f4d
Andrei Punko <andd3dfx@gmail.com>
2022-03-20 21:30:15 +0300

    Add separate template for index.html

--- generate.bat
+++ generate.bat
@@ -1,7 +1,7 @@
 
 del *.html
 
-java -jar bin/github-pages-content-generator.jar src/citations.txt bin/template.html index.html
+java -jar bin/github-pages-content-generator.jar src/citations.txt bin/template-index.html index.html
 java -jar bin/github-pages-content-generator.jar src/novij-zavet-o-carstve-nebesnom.txt bin/template.html novij-zavet-o-carstve-nebesnom.html
 java -jar bin/github-pages-content-generator.jar src/skolko-let.txt bin/template.html skolko-let.html
 java -jar bin/github-pages-content-generator.jar src/apologia-pravoslavia.txt bin/template.html apologia-pravoslavia.html
