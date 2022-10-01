8c2fde93df464570a715225902683c2822be1c61
Andrei Punko <andd3dfx@gmail.com>
2022-10-01 15:20:38 +0300

    Add loop into generation script
    According to https://stackoverflow.com/questions/138497/iterate-all-files-in-a-directory-using-a-for-loop

--- generate.bat
+++ generate.bat
@@ -1,8 +1,10 @@
 
 del *.html
 
+@ECHO OFF
+for %%f in (src\*.txt) do (
+  java -jar bin/github-pages-content-generator.jar %%f templates/template.html "%%~nf.html"
+)
+
+del citations.html
 java -jar bin/github-pages-content-generator.jar src/citations.txt templates/template-index.html index.html
-java -jar bin/github-pages-content-generator.jar src/novij-zavet-o-carstve-nebesnom.txt templates/template.html novij-zavet-o-carstve-nebesnom.html
-java -jar bin/github-pages-content-generator.jar src/skolko-let.txt templates/template.html skolko-let.html
-java -jar bin/github-pages-content-generator.jar src/monastery-requisites.txt templates/template.html monastery-requisites.html
-java -jar bin/github-pages-content-generator.jar src/volya-Gospodnia.txt templates/template.html volya-Gospodnia.html
