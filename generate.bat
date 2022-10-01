077d292b7ceda4cdeb6a2e1cf093f4475a0a9c60
Andrei Punko <andd3dfx@gmail.com>
2022-10-01 15:00:52 +0300

    Move templates to separate folder

--- generate.bat
+++ generate.bat
@@ -1,8 +1,8 @@
 
 del *.html
 
-java -jar bin/github-pages-content-generator.jar src/citations.txt bin/template-index.html index.html
-java -jar bin/github-pages-content-generator.jar src/novij-zavet-o-carstve-nebesnom.txt bin/template.html novij-zavet-o-carstve-nebesnom.html
-java -jar bin/github-pages-content-generator.jar src/skolko-let.txt bin/template.html skolko-let.html
-java -jar bin/github-pages-content-generator.jar src/monastery-requisites.txt bin/template.html monastery-requisites.html
-java -jar bin/github-pages-content-generator.jar src/volya-Gospodnia.txt bin/template.html volya-Gospodnia.html
+java -jar bin/github-pages-content-generator.jar src/citations.txt templates/template-index.html index.html
+java -jar bin/github-pages-content-generator.jar src/novij-zavet-o-carstve-nebesnom.txt templates/template.html novij-zavet-o-carstve-nebesnom.html
+java -jar bin/github-pages-content-generator.jar src/skolko-let.txt templates/template.html skolko-let.html
+java -jar bin/github-pages-content-generator.jar src/monastery-requisites.txt templates/template.html monastery-requisites.html
+java -jar bin/github-pages-content-generator.jar src/volya-Gospodnia.txt templates/template.html volya-Gospodnia.html
